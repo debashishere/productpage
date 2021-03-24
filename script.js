80f7ee5d82eab4673423d3c8e44deb15af7f1ab3
debashishere <debashishere007@gmail.com>
2021-03-24 12:59:00 +0530

    header, aside, product card style improved

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 $(document).ready(() => {
-    console.log("ready");
+    $('.hero-container a[href]').click(function (e) { e.preventDefault(); });
+
     $('.search').on("click", (event) => {
         event.preventDefault();
         const parent = $('.search').parent();
@@ -16,5 +17,26 @@ $(document).ready(() => {
     $('.close-btn').click((event) => {
         $('.nav-items').removeClass('active');
         $('.nav-overlay').removeClass('active');
+
     })
+
+    $('.card').hover(function (event) {
+        $(this).css({
+            'box-shadow': '8px -8px 30px 5px rgba(66, 66, 66, 0.25)',
+            '-webkit-box-shadow': '8px -8px 30px 5px rgba(66, 66, 66, 0.25)',
+            '-moz-box-shadow': '8px -8px 30px 5px rgba(66, 66, 66, 0.25)'
+        }).animate({ height: '300px' });
+        $(this).children(".addto-cart").css({ 'height': '68px', 'opacity': '1' })
+
+    }, function () {
+        $(this).css({
+            'box-shadow': ' 8px -8px 30px 5px rgba(66, 66, 66, 0.01)',
+            '-webkit-box-shadow': '8px -8px 30px 5px rgba(66, 66, 66, 0.01)',
+            '-moz-box-shadow': '8px -8px 30px 5px rgba(66, 66, 66, 0.01)'
+        }).animate({ height: '215px' });
+        $(this).children(".addto-cart").css({ 'height': '0%', 'opacity': '0' })
+
+    });
+
+
 })
